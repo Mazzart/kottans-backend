fd32745a7bf4d3f09a1f999687c5ed7088c3f6fa
Mazzart <am.magicsmile@gmail.com>
2019-12-07 10:34:22 +0200

    Update SQL SELECT statement

--- sql_basics/select.sql
+++ sql_basics/select.sql
@@ -1,9 +1,9 @@
 SELECT
   person.first_name,
   person.last_name,
-  COUNT(DISTINCT order.id) as total_orders,
-  SUM(order_item.quantity) as total_items_bought,
-  SUM(order_item.quantity * item.price - order_item.discount) as total_money_spent
+  COUNT(DISTINCT order.id) AS total_orders,
+  SUM(order_item.quantity) AS total_items_bought,
+  SUM(order_item.quantity * item.price - order_item.discount) AS total_money_spent
 FROM person
 LEFT JOIN `order` ON person.id = `order`.person_id
 LEFT JOIN order_item ON `order`.id = order_item.order_id
